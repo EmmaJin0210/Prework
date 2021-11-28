00cf552524fe01360f75c5d4914d9840a496d1c4
Emma Jin <mjin2@swarthmore.edu>
2021-11-28 18:45:02 -0500

    updating

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -8,34 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "Tip Calculator"
-
     }
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let defaults = UserDefaults.standard
+        let tipOne = defaults.string(forKey: "tipOneValue") ?? "15"
+        let tipTwo = defaults.string(forKey: "tipTwoValue") ?? "18"
+        let tipThree = defaults.string(forKey: "tipThreeValue") ?? "20"
+        print(tipOne, tipTwo, tipThree)
+        tipControl.setTitle(tipOne + "%", forSegmentAt: 0)
+        tipControl.setTitle(tipTwo + "%", forSegmentAt: 1)
+        tipControl.setTitle(tipThree + "%", forSegmentAt: 2)
+        
         let newInd = defaults.integer(forKey: "DefaultTipIndex")
         tipControl.selectedSegmentIndex = newInd
         calculateTip(super.viewWillAppear(animated))
         
     }
 
+    @IBAction func animateBill(_ sender: Any) {
+        billAmountTextField.shake()
+    }
     
     @IBAction func calculateTip(_ sender: Any) {
+        print("in calculate")
         let bill = Double(billAmountTextField.text!) ?? 0
-        let tipPercentages = [0.15, 0.18, 0.2]
+        var tipOne = tipControl.titleForSegment(at: 0) ?? "15"
+        tipOne.removeLast()
+        let tipOneVal = (Double(tipOne) ?? 15.0) * 0.01
+        var tipTwo = tipControl.titleForSegment(at: 1) ?? "18"
+        tipTwo.removeLast()
+        let tipTwoVal = (Double(tipTwo) ?? 18.0) * 0.01
+        var tipThree = tipControl.titleForSegment(at: 2) ?? "20"
+        tipThree.removeLast()
+        let tipThreeVal = (Double(tipThree) ?? 20.0) * 0.01
+        let tipPercentages = [tipOneVal, tipTwoVal, tipThreeVal]
+        print(tipPercentages)
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
+    
 }
 
+extension UITextField {
+    func shake() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        animation.fromValue = CGPoint(x: self.center.x - 4.0, y: self.center.y)
+        animation.toValue = CGPoint(x: self.center.x + 4.0, y: self.center.y)
+        layer.add(animation, forKey: "position")
+    }
+}
--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var tipOneValue: UITextField!
+    @IBOutlet weak var tipTwoValue: UITextField!
+    @IBOutlet weak var tipThreeValue: UITextField!
     @IBOutlet weak var defaultTipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let defaults = UserDefaults.standard
+        let oldTipOne = defaults.string(forKey: "tipOneValue") ?? "15"
+        let oldTipTwo = defaults.string(forKey: "tipTwoValue") ?? "18"
+        let oldTipThree = defaults.string(forKey: "tipThreeValue") ?? "20"
+        tipOneValue.text = oldTipOne
+        tipTwoValue.text = oldTipTwo
+        tipThreeValue.text = oldTipThree
+        defaultTipControl.setTitle(oldTipOne + "%", forSegmentAt: 0)
+        defaultTipControl.setTitle(oldTipTwo + "%", forSegmentAt: 1)
+        defaultTipControl.setTitle(oldTipThree + "%", forSegmentAt: 2)
+        
+        defaults.synchronize()
+        
     }
     //
     @IBAction func setDefaultTip(_ sender: Any) {
@@ -23,6 +39,28 @@ class SettingsViewController: UIViewController {
         defaults.synchronize()
     }
 
+
+    @IBAction func setTipOne(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        let newValue = tipOneValue.text
+        defaults.set(newValue, forKey: "tipOneValue")
+        defaults.synchronize()
+    }
+    
+
+    @IBAction func setTipTwo(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        let newValue = tipTwoValue.text
+        defaults.set(newValue, forKey: "tipTwoValue")
+        defaults.synchronize()
+    }
+    
+    @IBAction func setTipThree(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        let newValue = tipThreeValue.text
+        defaults.set(newValue, forKey: "tipThreeValue")
+        defaults.synchronize()
+    }
     
     
     /*    // MARK: - Navigation
